8f49d9a35214694ab1d23769fd1b27305e8415dd
Katarzyna <katarzyna.glab@schibsted.pl>
2016-10-29 11:22:24 +0200

    add front

--- src/main/java/hackaton/me/organize/OrganizeMeApp.java
+++ src/main/java/hackaton/me/organize/OrganizeMeApp.java
@@ -2,6 +2,8 @@ package hackaton.me.organize;
 
 import hackaton.me.organize.config.OrganizeMeConfig;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class OrganizeMeApp extends Application<OrganizeMeConfig> {
@@ -11,7 +13,13 @@ public class OrganizeMeApp extends Application<OrganizeMeConfig> {
     }
 
     @Override
+    public void initialize(final Bootstrap<OrganizeMeConfig> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle());
+    }
+
+    @Override
     public void run(final OrganizeMeConfig config, final Environment environment) throws Exception {
+        environment.jersey().setUrlPattern("/app/*");
         config.addResources(environment);
     }
 }
